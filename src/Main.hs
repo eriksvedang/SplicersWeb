965035059689f11c31d8b5172ecb94677034c659
Erik Svedäng <erik.svedang@gmail.com>
2015-08-01 23:51:03 +0200

    starting to work with lucid

--- src/Main.hs
+++ src/Main.hs
@@ -21,41 +21,57 @@ main :: IO ()
 main = do
   (Just port) <- lookupEnv "PORT"
   runSpock (read port) $ spockT id $ do
-    get root $ html $ toStrict $ renderText $ renderFrontPage
-    get ("hello" <//> var) hello
-    get ("files" <//> var) (\name -> file "A file..?" ("./files/" ++ name))
-    get "db" $ do
-      liftIO temp
-      (text "performed db stuff")
-    get "cards" $ do
-      cards <- liftIO getCards
-      renderCards cards
-    get "add-card" $ do
-      html "<form action='submit-card'>Title: <input type='text' name='title'><br>Rules text: <input type='text' name='rules'><br><input type='submit' value='Submit'></form>"
+    get root $ lucidToSpock renderFrontPage
+    get ("files" <//> var) getFile
+    get "cards" $ do cards <- liftIO getCards
+                     lucidToSpock $ renderCards cards
+    get "add-card" $ lucidToSpock renderAddCard
     get "submit-card" $ do
       (Just title) <- param "title"
       (Just rules) <- param "rules"
       liftIO (addCard title rules)
-      html $ "Added card '" <> title <> "'<br><a href='/cards'>Card list</a>"
+      lucidToSpock (renderSubmitCard title rules)
 
-render t = html $ toStrict $ renderText t
+getFile name = file (pack name) ("./files/" ++ name)
 
-renderFrontPage = do head_ $ do link_ [rel_ "stylesheet", type_ "text/css", href_ "/files/styles.css"]
-                                link_ [rel_ "stylesheet", type_ "text/css", href_ "/files/card.css"]
-                                link_ [href_ "http://fonts.googleapis.com/css?family=Karla:400,700,400italic,700italic",
-                                       rel_ "stylesheet",
-                                       type_ "text/css"]
-                                body_ $ do h1_ "Splicers"
-                                           h2_ "An open source collectible card game"
-                                           p_ "Yeah, it's pretty cool."
-                                           ---a_ [href_ "/cards"] "Cards
+lucidToSpock :: MonadIO m => Html () -> ActionT m a
+lucidToSpock t = html $ toStrict $ renderText t
 
-hello :: MonadIO m => String -> ActionT m a
-hello name = render $ do (h1_ $ "Hello!") :: Html ()
+css :: Text -> Html ()
+css name = link_ [rel_ "stylesheet", type_ "text/css", href_ name]
 
-renderCards :: MonadIO m => [Card] -> ActionT m a
-renderCards cards = do
-  html $ Text.unlines (map renderCard cards)
+allCSS :: Html ()
+allCSS = do (css "/files/styles.css")
+            (css "/files/card.css")
+            (css "http://fonts.googleapis.com/css?family=Karla:400,700,400italic,700italic")
 
-renderCard :: Card -> Text
-renderCard card = "<ul>" <> title card <> " - " <> rules card <> "</ul>"
+renderPage :: Html () -> Html ()
+renderPage body = do head_ $ allCSS
+                     body_ $ body
+
+renderFrontPage :: Html ()
+renderFrontPage = renderPage $ do h1_ "Splicers"
+                                  h2_ "An open source collectible card game"
+                                  p_ "Yeah, it's pretty cool."
+                                  a_ [href_ "/cards"] "Cards"
+
+renderCards cards = mapM_ renderCard cards
+
+renderCard :: Card -> Html ()
+renderCard card = do div_ $ do p_ $ toHtml (title card)
+                               p_ $ toHtml (rules card)
+
+renderAddCard =
+  renderPage $ do
+    form_ [action_ "submit-card"] $ do
+      div_ "Title:"
+      input_ [type_ "text", name_ "title"]
+      div_ "Rules text:"
+      input_ [type_ "text", name_ "rules"]
+      br_ []
+      input_ [type_ "submit", value_ "Submit"]
+
+renderSubmitCard :: Text -> Text -> Html ()
+renderSubmitCard title rules = renderPage $ do p_ $ toHtml $ "Title: " <> title
+                                               p_ $ toHtml $ "Rules: " <> rules
+
