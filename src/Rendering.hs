55272af0118c2fe3b2aded0d7dfc6551fe03355a
catnipped <ossianboren@gmail.com>
2016-12-15 16:45:08 +0100

    hide deckid

--- src/Rendering.hs
+++ src/Rendering.hs
@@ -282,11 +282,11 @@ renderDeckPage activeDeck deck cards = do
     div_ [class_ "window"] $ do
       div_ [class_ "content deckedit"] $ do
         input_ [id_ "deckname", class_ "randomcolor", value_ (deckName deck), name_ "deckname"]
-        input_ [name_ "deckid", style_ "float:right; background: black; color: white;", value_ ((pack . show . deckId) deck), readonly_ ""]
+        input_ [name_ "deckid", style_ "display:none", value_ ((pack . show . deckId) deck), readonly_ ""]
         span_ [class_ "whileediting", style_ "display:none;"] (toHtml "Currently editing. Click a card to remove it from your deck. Click the title to change it. ")
         a_ [class_ "whileediting button", style_ "display:none;",  href_ "#", onclick_ "removeCookie()"] "Finish editing"
-        a_ [class_ "whileediting button", style_ "display:none;",  href_ "#", onclick_ "deleteDeck()"] "Delete Deck"
-        a_ [href_ (T.append "/edit-deck/" ((pack . show . deckId) deck))] $ do
+        a_ [class_ "button", style_ "display:none;",  href_ "#", onclick_ "deleteDeck()"] "Delete Deck"
+        a_ [class_ "whileediting", style_ "display:none;", href_ "/cards"] $ do
                                                                      div_ [class_ "add"] $ do
                                                                        span_ [] (toHtml "Add more cards to deck")
         mapM_ (renderCard InDeckSelection) cards
